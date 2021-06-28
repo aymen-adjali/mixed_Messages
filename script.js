e72cb3daafa0c5a1fd1d88cc7abc9d9176f8f5c4
Aymen Adjali <aymen_adjali@hotmail.co.uk>
2021-06-28 12:42:28 +0100

    Added data structures to hold my messages and function to return a random word from a given array.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello test");
+const firstArray = ["Hello", "Hi", "Howdy", "Greetings", "Good day"];
+const secondArray = ["user", "person", "human", "earthling", "mortal"];
+const ThridArray = ["have a great day", "you will shine today", "you are going to smash today", "you are important", "remember your worth"];
+
+
+function pickRandomWordInArray (array) {
+    let randomWord;
+    const randomIndex = Math.floor(Math.random() * array.length);
+
+    randomWord = array[randomIndex];
+    return randomWord;
+}
